08bcd4466c6be81dda267778050d2c3374c22163
Vedx0609 <vedant.s.varma@gmail.com>
2025-03-15 17:51:32 +0530

    Modifying hello.cpp and performing Task 2

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "Hello I am Vedant Varma PES1UG22CS681\n";
+  cout << "I have successfully built and run this\n";
   return 0;
 }
